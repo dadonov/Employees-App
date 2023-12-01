95be7c8e72ddb0226d35ebed20e3a98ca8eff9c0
Igor Dadonov <dadonov.igor@gmail.com>
2023-12-01 16:48:50 +0400

    feat: add logic

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,24 +1,49 @@
+import { Component } from 'react';
 import './employees-add-form.css';
 
-const EmployeesAddForm = () => {
-  return (
-    <div className="app-add-form">
-      <h3>Добавить нового сотрудника</h3>
-      <form action="" className="add-form d-flex">
-        <input
-          type="text"
-          placeholder="Имя нового соотрудника"
-          className="form-control new-post-label"
-        />
-        <input
-          type="text"
-          placeholder="Заработная плата в $"
-          className="form-control new-post-label"
-        />
-        <button className="btn btn-outline-light">Добавить</button>
-      </form>
-    </div>
-  );
-};
+class EmployeesAddForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      salary: '',
+    };
+  }
+
+  onValueChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  render() {
+    const { name, salary } = this.state;
+
+    return (
+      <div className="app-add-form">
+        <h3>Добавить нового сотрудника</h3>
+        <form className="add-form d-flex">
+          <input
+            type="text"
+            name="name"
+            value={name}
+            placeholder="Имя нового соотрудника"
+            className="form-control new-post-label"
+            onChange={this.onValueChange}
+          />
+          <input
+            type="number"
+            name="salary"
+            value={salary}
+            placeholder="Заработная плата в $"
+            className="form-control new-post-label"
+            onChange={this.onValueChange}
+          />
+          <button className="btn btn-outline-light">Добавить</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default EmployeesAddForm;
